1a273f6f759733789848044709109a1aa60b8113
Abdoulaye Chérif Touré <abdoulaye.cherif.toure@altesse.com>
2021-06-01 17:06:26 +0200

    feat: using pm2 for deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps : [{
+    script: 'server.js',
+    watch: '.'
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
+
+  deploy : {
+    production : {
+      user : 'ubuntu',
+      host : '51.178.53.176',
+      ref  : 'origin/main',
+      repo : 'git@github.com:cherift/reserv-hotel.git',
+      path : '/home/ubuntu/reserv-hotel',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
