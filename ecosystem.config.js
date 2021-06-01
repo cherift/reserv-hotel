f357257e05f28a4a012c157ea6016860a9150337
Abdoulaye Chérif Touré <abdoulaye.cherif.toure@altesse.com>
2021-06-01 17:15:05 +0200

    feat: set git http repo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       user : 'ubuntu',
       host : '51.178.53.176',
       ref  : 'origin/main',
-      repo : 'git@github.com:cherift/reserv-hotel.git',
+      repo : 'https://github.com/cherift/reserv-hotel.git',
       path : '/home/ubuntu/reserv-hotel',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
